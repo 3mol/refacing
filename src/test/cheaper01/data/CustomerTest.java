3b450849f799fce532df7b71a0f575959b8d131f
Hu yujing <hyj1127176797@hotmail.com>
2021-04-24 22:46:03 +0800

    [code 1.2] 1. 添加新品且租期为1的测试用例 2. 删除无效的测试用例

--- src/test/cheaper01/data/CustomerTest.java
+++ src/test/cheaper01/data/CustomerTest.java
@@ -14,24 +14,6 @@ public class CustomerTest {
   }
 
   @Test
-  public void statement1() {
-    Movie godzilla = new Movie("哥斯拉", Movie.REGULAR);
-    Movie kingKong = new Movie("金刚", Movie.REGULAR);
-    Movie GodzillaVsKong = new Movie("哥斯拉大战金刚", Movie.NEW_RELEASE);
-    Movie teletubbies = new Movie("天线宝宝", Movie.CHILDREN);
-
-    Rental godzilla3DaysRented = new Rental(godzilla, 3);
-    Rental teletubbies3DaysRented = new Rental(teletubbies, 3);
-    kang.addRental(godzilla3DaysRented);
-    kang.addRental(teletubbies3DaysRented);
-    assertEquals("kang的租赁记录\n" +
-      "\t哥斯拉\t3.5\n" +
-      "\t天线宝宝\t1.5\n" +
-      "您欠下的金额为5.0\n" +
-      "您获得了2积分点", kang.statement());
-  }
-
-  @Test
   public void statement_regularMovie3DaysRental() {
     Movie godzilla = new Movie("哥斯拉", Movie.REGULAR);
 
